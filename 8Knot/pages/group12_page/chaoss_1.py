54f0565b6c26c9f8bd263854c493efb91e46a9a2
JackRiebel <jack.riebel@gmail.com>
2023-11-14 14:31:13 -0600

    added another viz

--- 8Knot/pages/group12_page/chaoss_1.py
+++ 8Knot/pages/group12_page/chaoss_1.py
@@ -8,6 +8,7 @@ from .visualizations.bus_factor import gc_bus_factor
 from .visualizations.time_to_first_response import gc_time_to_first_response
 from .visualizations.release_frequency import gc_release_frequency
 from .visualizations.change_request_closure_ratio import gc_change_request_closure_ratio
+from .visualizations.change_request_review_duration import gc_change_request_review_duration
 
 warnings.filterwarnings("ignore")
 
@@ -20,7 +21,8 @@ layout = dbc.Container(
                 dbc.Col(gc_time_to_first_response, width=6),
                 dbc.Col(gc_bus_factor, width=6),
                 dbc.Col(gc_release_frequency, width=6),
-                dbc.Col(gc_change_request_closure_ratio, width=6)
+                dbc.Col(gc_change_request_closure_ratio, width=6),
+                dbc.Col(gc_change_request_review_duration, width=6)
             ],
             align="center",
             style={"marginBottom": ".5%"},
